87522b5d6c540be14b0075e9cef86dc7be21da76
MaiYiBuMaiMaiZi <1242970421@qq.com>
2023-05-06 19:37:37 +0800

    生成随机数

--- 5.4/random.java
+++ 5.4/random.java
@@ -1,9 +1,22 @@
+import java.util.Arrays;
+
 public class random {
     public static void main(String[] args) {
         int[] arr = new int[10];
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) (Math.random());
-            System.out.println(arr[i] + "  ");
+            arr[i] = (int) (Math.random() * 100) + 1;
         }
+        System.out.println("排序前：" + Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("排序后：" + Arrays.toString(arr));
     }
 }
